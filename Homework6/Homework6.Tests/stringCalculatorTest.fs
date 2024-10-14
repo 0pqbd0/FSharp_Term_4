322676a19a78696279d4afa060d6d483db3f98ca
Artem Mikulskii <a.mikulskiy@versta24.ru>
2024-10-14 22:10:41 +0300

    add sixth homework

---
+++ Homework6/Homework6.Tests/stringCalculatorTest.fs
@@ -0,0 +1,37 @@
+﻿module stringCalculatorTest
+
+open FsUnit
+open NUnit.Framework
+open stringCalculator
+
+[<Test>]
+let stringCalculatorWithTwoCorrectNumbers () =
+    let result = 
+        calculate {
+        let! x = "15"
+        let! y = "79"
+        return x + y
+    }
+    result |> should equal (Some 94)
+
+[<Test>]
+let stringCalculatorWithThreeCorrectNumbers () =
+    let calculate = StringCalculator()
+
+    let result = calculate {
+        let! x = "1500"
+        let! y = "350"
+        let! z = "70"
+        let w = x + y + z
+        return w
+    }
+    result |> should equal (Some 1920) 
+
+[<Test>]
+let stringCalculatorWithIncorrectNumbers () =
+    let result = calculate {
+        let! x = "1"
+        let! y = "two"
+        return x + y
+    }
+    result |> should equal None
---
+++ Homework6/Homework6/stringCalculator.fs
@@ -0,0 +1,11 @@
+﻿module stringCalculator
+
+type StringCalculator() =
+    member this.Bind(x: string, f) =
+        match System.Int32.TryParse x with
+        | true, value -> f value
+        | false, _ -> None
+
+    member this.Return x = Some(x)
+
+let calculate = StringCalculator()

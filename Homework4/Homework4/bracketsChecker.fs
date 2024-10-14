eb3d4714d90855b5945d91fae16adda5b43e88d1
Artem Mikulskii <a.mikulskiy@versta24.ru>
2024-10-14 22:07:53 +0300

    add fourth homework

---
+++ Homework4/Homework4/bracketsChecker.fs
@@ -0,0 +1,15 @@
+﻿module bracketsChecker
+
+(*   Скобочная последовательность   *)
+
+let checkBrackets (s: string) =
+    let rec check lst chars =
+        match chars, lst with
+        | [], [] -> true
+        | [], _ -> false
+        | c::cs, _ when c = '(' || c = '[' || c = '{' -> check (c::lst) cs
+        | ')'::cs, '('::ls -> check ls cs 
+        | ']'::cs, '['::ls -> check ls cs 
+        | '}'::cs, '{'::ls -> check ls cs
+        | _ -> false
+    s |> Seq.toList |> check []
